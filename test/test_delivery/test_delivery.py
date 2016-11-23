ae6f763b85c7bfad89ac103c1d0208c568acb8bb
Bartłomiej Kurek <bartlomiej.kurek@messente.com>
2016-11-23 17:13:22 +0200

    delivery test

---
+++ test/test_delivery/test_delivery.py
@@ -0,0 +1,40 @@
+import responses
+
+from test import utils
+
+from messente.api import delivery
+from messente.api import Response
+
+
+api = delivery.DeliveryAPI(api_url=utils.TEST_DLR_URL)
+
+
+@responses.activate
+def test_dlr():
+    status = "DELIVERED"
+    text = "OK %s" % status
+    responses.add_callback(
+        responses.GET, utils.TEST_DLR_ANY_URL,
+        callback=utils.mock_response(200, text),
+    )
+
+    r = api.get_dlr_response("smsid")
+    assert isinstance(r, Response)
+    assert isinstance(r, delivery.DeliveryResponse)
+    assert r.get_raw_text() == text
+    assert r.get_delivery_status() == "DELIVERED"
+
+
+@responses.activate
+def test_dlr_failed():
+    text = "FAILED 102"
+    responses.add_callback(
+        responses.GET, utils.TEST_DLR_ANY_URL,
+        callback=utils.mock_response(200, text),
+    )
+
+    r = api.get_dlr_response("smsid")
+    assert isinstance(r, Response)
+    assert isinstance(r, delivery.DeliveryResponse)
+    assert r.get_delivery_status() is None
+    assert not r.is_ok()
